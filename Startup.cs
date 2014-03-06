4d9987c52fd6153bba82ed59bf28163816c168f3
mikenimegeers <mikenimegeers@gmail.com>
2014-03-05 19:04:17 -0700

    vanilla

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AOD_MVC5.Startup))]
+namespace AOD_MVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
